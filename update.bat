66341e6d699555acea0728ea4a58dbe13ea85295
timvisee <timvisee@gmail.com>
2017-02-21 22:08:24 +0100

    Reworked update script for Windows

--- update.bat
+++ update.bat
@@ -1,8 +1,16 @@
-echo "Fetching new updates..."
+@echo off
+cls
+
+title Updating container...
+
+# Header
+@echo "Updating container scripts..."
+
+@echo "Fetching new updates..."
 git fetch
 
-echo "Checking out latest version..."
+@echo "Checking out latest version..."
 git checkout origin master
 
-echo "Done"
+echo "Update done."
 
