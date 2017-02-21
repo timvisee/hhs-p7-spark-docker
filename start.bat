282e2d6257c996dc8e9fd4b5453dccd64c2f9e75
timvisee <timvisee@gmail.com>
2017-02-21 14:38:06 +0100

    Add start scripts for Docker on Windows

---
+++ start.bat
@@ -0,0 +1,4 @@
+rem Start the docker container and detach
+echo "Starting Spark container using Docker..."
+docker-compose up -d
+echo "Done"
--- stop.bat
+++ stop.bat
@@ -1,6 +1,4 @@
-#!/bin/bash
-
-# Start the docker container and detach
+rem Start the docker container and detach
 echo "Stopping running Spark container in Docker..."
 sudo docker-compose stop
 echo "Done"
