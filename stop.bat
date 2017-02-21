d9db4746cc3d404e531f2da6fb1ba427b8742e40
timvisee <timvisee@gmail.com>
2017-02-21 22:20:17 +0100

    Create constant with project name in Windows stop script

--- stop.bat
+++ stop.bat
@@ -3,8 +3,11 @@ cls
 
 title Stopping Spark container...
 
+# Script constants
+SET PROJECT_NAME=spark
+
 # Stop the docker container and detach
 @echo "Stopping running Spark container in Docker..."
-docker-compose -p spark stop
+docker-compose -p "%PROJECT_NAME%" stop
 @echo "All Spark containers have been stopped."
 
