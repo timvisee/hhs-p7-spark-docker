ffc3e1b45d238067a4eb35fe409f1f1e2b5ab38e
timvisee <timvisee@gmail.com>
2017-02-22 17:02:23 +0100

    Update installation scripts

--- install.bat
+++ install.bat
@@ -3,21 +3,20 @@
 title Installing container...
 
 REM Script constants
-SET PROJECT_WORK_DIR=.\work
-SET PROJECT_RESOURCE_DIR=.\res
+SET PROJECT_NOTEBOOK_DIR=.\notebook
 
 REM Header
 @echo Installation script.
 
 REM Pull the docker image
-@echo Updating container image...
+@echo Pulling container image...
 docker-compose -f ".\docker-compose.yml" pull
 
-REM Create the work directory
-IF NOT EXIST "%PROJECT_WORK_DIR%" (
-    @echo Createing working directory...
-    mkdir "%PROJECT_WORK_DIR%"
-    icacls "%PROJECT_WORK_DIR%" /grant Users:F
+REM Create the notebook directory
+IF NOT EXIST "%PROJECT_NOTEBOOK_DIR%" (
+    @echo Creating notebook  directory...
+    mkdir "%PROJECT_NOTEBOOK_DIR%"
+    icacls "%PROJECT_NOTEBOOK_DIR%" /grant Users:F
 )
 
 @echo Installation is done.
