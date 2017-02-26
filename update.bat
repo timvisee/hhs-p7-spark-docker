84e8ddaaa6cb4c321b7e3724421e9fdbe4c2c583
timvisee <timvisee@gmail.com>
2017-02-26 14:59:51 +0100

    Reinstall containers after update

--- update.bat
+++ update.bat
@@ -8,11 +8,15 @@ REM Header
 REM Stop running containers
 call stop.bat
 
-@echo Fetching new updates...
+@echo Fetching new updates from repository...
 git fetch
 
 @echo Checking out latest version...
 git checkout origin/master
 
+echo "Running installation..."
+call install.bat
+
+echo Use the 'start.bat' command to start the updated container.
 echo Update done.
 
