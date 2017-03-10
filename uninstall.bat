7793e869407ee586c2e23e76354dab84f335b13c
timvisee <timvisee@gmail.com>
2017-03-10 14:07:15 +0100

    Remove Windows VM scripts from uninstall script

--- uninstall.bat
+++ uninstall.bat
@@ -41,77 +41,21 @@ pause
 REM Stop any running containers
 call stop.bat
 
-REM Validate Docker is installed correctly
-@WHERE docker-machinetest
-IF %ERRORLEVEL% NEQ 0 (
-    REM Make sure docker is installed
-    @WHERE docker
-    IF %ERRORLEVEL% NEQ 0 (
-        REM Show an error message
-        @echo.
-        @echo ERROR: Docker hasn't been installed correctly.
-        @echo        The command 'docker' isn't recognized.
-        @echo        Because of this, the container can't be uninstalled.
-        @echo        Please read the README and follow the installation instructions.
-
-        REM Open the README in the browser
-        start "%README_URL%"
-        
-        exit
-    )
-
-    REM Make sure docker-compose is installed
-    @WHERE docker-compose
-    IF %ERRORLEVEL% NEQ 0 (
-        REM Show an error message
-        @echo.
-        @echo ERROR: Docker hasn't been installed correctly.
-        @echo        The command 'docker-compose' isn't recognized.
-        @echo        Because of this, the container can't be uninstalled.
-        @echo        Please read the README and follow the installation instructions.
-
-        REM Open the README in the browser
-        start "%README_URL%"
-        
-        exit
-    )
-)
-
 REM Remove the user data
 @echo Uninstalling user data...
 rmdir "%NOTEBOOK_DIR%" /s
 @echo User data uninstalled!
 @echo.
 
-REM Check whether to use regular docker or docker tooblox
-WHERE docker-machine
-IF %ERRORLEVEL% NEQ 0 (
-    REM Show a status message
-    @echo Switched to regular Docker, not Docker toolbox
+REM Show a status message
+@echo Switched to regular Docker, not Docker toolbox
 
-    REM Remove the container and image
-    @echo Uninstalling container...
-    docker-compose -f "docker-compose.yml" -p "%PROJECT_NAME%" rm -f
-    docker rmi "%DOCKER_IMAGE%"
-    @echo Container uninstalled!
-    @echo.
-
-) ELSE (
-    REM Show a status message
-    @echo Switched to Docker toolbox using a VM, not regular Docker
-
-    REM Install the virtual machine if it doesn't exist
-    docker-machine ls | find /i "%MACHINE_NAME%"
-    IF NOT errorlevel 1 (
-        REM Uninstall the virtual machine
-        @echo Uninstall the virtual machine for Docker...
-        docker-machine rm %MACHINE_NAME%
-
-    ) ELSE (
-        @echo Virtual machine for Docker could not be found. 
-        @echo It has probably already been uninstalled.
-    )
-)
+REM Remove the container and image
+@echo Uninstalling container...
+docker-compose -f "docker-compose.yml" -p "%PROJECT_NAME%" rm -f
+docker rmi "%DOCKER_IMAGE%"
+@echo Container uninstalled!
+@echo.
 
 REM Uninstall/remove the repository
 @echo Uninstalling repository...
